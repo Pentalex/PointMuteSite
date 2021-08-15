f222b0e7b936dda9478a61e68f8b4d07f1e5caf7
Alex <aleksanderarentz@gmail.com>
2021-08-15 21:42:02 +0200

    trihard

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 var timerSocket = new WebSocket("ws://localhost:8080")
+const muteImg = document.getElementById('muteImg')
+
 timerSocket.onmessage = function (event) {
   console.log(event.data);
+  if(event.data.startsWith('settime')){
+  	muteImg.style.opacity = "1"
+  } else if(event.data.startsWith('unmute')){
+  	muteImg.style.opacity = "0"
+  }
 }
